6c6ca13d149d6e914798f5f8aab217dc97623918
Nick Qi <nickqi@tubi.tv>
2024-07-12 21:55:25 +0800

    feat: initialize tensor client

--- src/index.js
+++ src/index.js
@@ -2,8 +2,21 @@ require('dotenv').config();
 const axios = require('axios');
 
 const tensorClient = axios.create({
-  baseUrl: 'http://tensor.production-public.tubi.io',
-  header: {
-    Authorization: 'Bearer token',
+  baseURL: 'https://tensor.production-public.tubi.io',
+  headers: {
+    Authorization: `Bearer ${process.env.ACCESS_TOKEN}`,
   },
 });
+
+(async () => {
+  try {
+    const data = await tensorClient.get('/api/v3/homescreen');
+    console.log('data!!', data);
+  } catch (error) {
+    console.error('error!!', error);
+  }
+})();
+
+module.exports = {
+  tensorClient,
+};

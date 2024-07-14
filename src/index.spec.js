6f35ee95e2449419b2e9bd4227bba6113b8ac986
Nick Qi <nickqi@tubi.tv>
2024-07-14 18:24:10 +0800

    feat: optimize jest reporter for CI

--- src/index.spec.js
+++ src/index.spec.js
@@ -1,22 +1,48 @@
 const { clients } = require('.');
 
+const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+
 it('should return a list of clients', async () => {
   const keys = Object.keys(clients);
   expect(keys).toEqual(['account', 'tensor', 'search']);
 });
 
-it.each`
-  service      | pathname                | params
-  ${'account'} | ${'/user/settings'}     | ${undefined}
-  ${'tensor'}  | ${'/api/v3/homescreen'} | ${undefined}
-  ${'tensor'}  | ${'/api/v4/homescreen'} | ${undefined}
-`('should handle user deletion when service=$service and pathname=$pathname', async ({ service, pathname, params }) => {
-  expect.assertions(2);
-  try {
-    await clients[service].get(pathname, { params });
-  } catch (err) {
-    const { response } = err;
-    expect(response.status).toBe(401);
-    expect(response.data).toEqual({ code: 'USER_NOT_FOUND', message: 'User not found' });
-  }
+describe('check user deletion', () => {
+  let unexpectedResponse = {};
+
+  afterAll(() => {
+    if (Object.keys(unexpectedResponse).length) {
+      console.log('Unexpected responses:', unexpectedResponse);
+    }
+  });
+
+  it.each`
+    service      | pathname                         | params
+    ${'account'} | ${'/user/settings'}              | ${undefined}
+    ${'search'}  | ${'/api/v1/search'}              | ${(search = 'star trek')}
+    ${'tensor'}  | ${'/api/v2/homescreen'}          | ${undefined}
+    ${'tensor'}  | ${'/api/v3/homescreen'}          | ${undefined}
+    ${'tensor'}  | ${'/api/v4/homescreen'}          | ${undefined}
+    ${'tensor'}  | ${'/api/v2/containers/featured'} | ${undefined}
+    ${'tensor'}  | ${'/api/v3/containers/featured'} | ${undefined}
+    ${'tensor'}  | ${'/api/v4/containers/featured'} | ${undefined}
+  `(
+    'should handle user deletion when service=$service and pathname=$pathname',
+    async ({ service, pathname, params }) => {
+      expect.assertions(2);
+      await sleep(500);
+
+      try {
+        await clients[service].get(pathname, { params });
+      } catch (err) {
+        const { response } = err;
+        unexpectedResponse[`${service}:${pathname}`] = {
+          status: response.status,
+          data: response.data,
+        };
+        expect(response.status).toBe(401);
+        expect(response.data).toEqual({ code: 'USER_NOT_FOUND', message: 'User not found' });
+      }
+    },
+  );
 });

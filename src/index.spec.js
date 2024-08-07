9dd44022b092f46880bf274d84810ed42ba946d1
Nick Qi <nickqi@tubi.tv>
2024-08-07 10:59:00 +0800

    fix: undefined response status

--- src/index.spec.js
+++ src/index.spec.js
@@ -20,26 +20,26 @@ describe('check user deletion', () => {
   });
 
   it.each`
-    service         | pathname                              | params
-    ${'account'}    | ${'/user/settings'}                   | ${undefined}
-    ${'account'}    | ${'/user/check_birthday_info'}        | ${undefined}
-    ${'crm'}        | ${'//api/v1/collection/leaving_soon'} | ${undefined}
-    ${'epg'}        | ${'/content/epg/programming'}         | ${undefined}
-    ${'lishi'}      | ${'/api/v2/view_history'}             | ${undefined}
-    ${'search'}     | ${'/api/v1/search'}                   | ${{ search: 'star trek' }}
-    ${'tensor'}     | ${'/api/v2/containers/featured'}      | ${undefined}
-    ${'tensor'}     | ${'/api/v3/containers/featured'}      | ${undefined}
-    ${'tensor'}     | ${'/api/v4/containers/featured'}      | ${undefined}
-    ${'tensor'}     | ${'/api/v2/homescreen'}               | ${undefined}
-    ${'tensor'}     | ${'/api/v3/homescreen'}               | ${undefined}
-    ${'tensor'}     | ${'/api/v4/homescreen'}               | ${undefined}
-    ${'tensor'}     | ${'/api/v4/homescreen'}               | ${undefined}
-    ${'tensor'}     | ${'/api/v1/epg'}                      | ${undefined}
-    ${'tensor'}     | ${'/api/v1/live_programming'}         | ${undefined}
-    ${'tensor'}     | ${'/api/v1/pmr'}                      | ${undefined}
-    ${'tensor'}     | ${'/api/v1/scenes'}                   | ${undefined}
-    ${'user-queue'} | ${'/api/v1/linear_reminder'}          | ${undefined}
-    ${'user-queue'} | ${'/api/v2/queues'}                   | ${undefined}
+    service         | pathname                             | params
+    ${'account'}    | ${'/user/settings'}                  | ${undefined}
+    ${'account'}    | ${'/user/check_birthday_info'}       | ${undefined}
+    ${'crm'}        | ${'/api/v1/collection/leaving_soon'} | ${undefined}
+    ${'epg'}        | ${'/content/epg/programming'}        | ${undefined}
+    ${'lishi'}      | ${'/api/v2/view_history'}            | ${undefined}
+    ${'search'}     | ${'/api/v1/search'}                  | ${{ search: 'star trek' }}
+    ${'tensor'}     | ${'/api/v2/containers/featured'}     | ${undefined}
+    ${'tensor'}     | ${'/api/v3/containers/featured'}     | ${undefined}
+    ${'tensor'}     | ${'/api/v4/containers/featured'}     | ${undefined}
+    ${'tensor'}     | ${'/api/v2/homescreen'}              | ${undefined}
+    ${'tensor'}     | ${'/api/v3/homescreen'}              | ${undefined}
+    ${'tensor'}     | ${'/api/v4/homescreen'}              | ${undefined}
+    ${'tensor'}     | ${'/api/v4/homescreen'}              | ${undefined}
+    ${'tensor'}     | ${'/api/v1/epg'}                     | ${undefined}
+    ${'tensor'}     | ${'/api/v1/live_programming'}        | ${undefined}
+    ${'tensor'}     | ${'/api/v1/pmr'}                     | ${undefined}
+    ${'tensor'}     | ${'/api/v1/scenes'}                  | ${undefined}
+    ${'user-queue'} | ${'/api/v1/linear_reminder'}         | ${undefined}
+    ${'user-queue'} | ${'/api/v2/queues'}                  | ${undefined}
   `(
     'should handle user deletion when service=$service and pathname=$pathname',
     async ({ service, pathname, params }) => {
@@ -52,13 +52,13 @@ describe('check user deletion', () => {
         response = await clients[service].get(pathname, { params });
       } catch (err) {
         response = err.response;
-        expect(response.status).toBe(401);
-        expect(response.data).toEqual(expect.objectContaining({ code: 'USER_NOT_FOUND' }));
+        expect(response?.status).toBe(401);
+        expect(response?.data).toEqual(expect.objectContaining({ code: 'USER_NOT_FOUND' }));
         hasPassed = true;
       } finally {
         (hasPassed ? passedResponse : unexpectedResponse)[`${service}:${pathname}`] = {
-          status: response.status,
-          data: response.data,
+          status: response?.status,
+          data: response?.data,
         };
       }
     },
